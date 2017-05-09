84a363c545ad7621b6614e4d680da36e9658f5a5
Björn Fröhling <bjorn.frohling@qvik.fi>
2017-05-09 13:15:25 +0300

    Initial Commit

---
+++ UnitTestsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTestsExample
+//
+//  Created by Björn Fröhling on 09/05/2017.
+//  Copyright © 2017 Fröhling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
